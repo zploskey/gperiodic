1fc455bf45a1986128c5667b10e0f73a94f0aca6
loginov.alex.valer <loginov.alex.valer@0ad6494c-dd34-4e59-9d04-8dff0e227eb0>
2014-06-09 08:56:30 +0000

    - update all translations
    git-svn-id: http://svn.code.sf.net/p/gperiodic/code/trunk@23 0ad6494c-dd34-4e59-9d04-8dff0e227eb0

--- gperiodic.h
+++ gperiodic.h
@@ -98,11 +98,11 @@ int interactive_shell(void);
 /************************************************************************************************/
 
 #define PROGNAME "gperiodic"
-#define COPYRIGHT gettext("Copyright (C) 1999-2002 Kyle R. Burton, 2003-2007 Jonas Frantz, 2014 Alexey Loginov")
+#define COPYRIGHT "Copyright (C) 1999-2002 Kyle R. Burton, 2003-2007 Jonas Frantz, 2014 Alexey Loginov"
 #define COMMENTS gettext("This program shows the periodic table of elements and information about these elements")
 #define LICENSE gettext("gperiodic is free software; you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation; either version 2 of the License, or\n(at your option) any later version.\n\ngperiodic is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with this program; if not, write to the Free Software\nFoundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA\n")
 #define HOMEPAGEURL "http://code.google.com/p/gperiodic"
 #define HOMEPAGELABEL gettext("gperiodic homepage")
-#define AUTHORS {"Kyle R. Burton", "Jonas Frantz", "Alexey Loginov", "Piotr Najman", "Arno W. Peters", "Georges Khaznadar", "Mauricio Rivera", "Lalo Martins", "Sven Neumann", "Christian Klein", "Koen Kooi", "Ambrogio Oliva", "Felix Natter", "Michael Shigorin", "Costantino Ceoldo", "Arkadiusz Lipiec", "Sebastien Bacher", "Daniel Leidert", "Theppitak Karoonboonyanan", "Zach Ploskey", "Jon Abbott", "Andrew Dalke", "Gunner Poulsen", "Gonzalo Regueiro Iglesias", "Leandro Regueiro", "Andrea Tasso", "Osman Köşeli", "Ireneusz Imiolek", "Oleksandr Mishchenko", "Juan Rafael Fernández", "Fırat Kutlu", NULL}
+#define AUTHORS {"Kyle R. Burton", "Jonas Frantz", "Alexey Loginov", "Piotr Najman", "Arno W. Peters", "Georges Khaznadar", "Mauricio Rivera", "Lalo Martins", "Sven Neumann", "Christian Klein", "Koen Kooi", "Ambrogio Oliva", "Felix Natter", "Michael Shigorin", "Costantino Ceoldo", "Arkadiusz Lipiec", "Sebastien Bacher", "Daniel Leidert", "Theppitak Karoonboonyanan", "Zach Ploskey", "Jon Abbott", "Andrew Dalke", "Gunner Poulsen", "Gonzalo Regueiro Iglesias", "Leandro Regueiro", "Andrea Tasso", "Osman Köşeli", "Frederic Gobry", "Ireneusz Imiolek", "Oleksandr Mishchenko", "Juan Rafael Fernández", "Fırat Kutlu", NULL}
 
 
